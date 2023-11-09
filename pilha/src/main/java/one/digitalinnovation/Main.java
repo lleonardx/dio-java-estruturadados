e7ab27de16f383be30b549a8244eb9949d41b3d0
lleonardx <lleonardx.alves@gmail.com>
2023-11-09 16:55:59 -0400

    updates das pastas

--- pilha/src/main/java/one/digitalinnovation/Main.java
+++ pilha/src/main/java/one/digitalinnovation/Main.java
@@ -10,16 +10,13 @@ public class Main {
         minhaPilha.push(new No(4));
         minhaPilha.push(new No(5));
         minhaPilha.push(new No(6));
-
+        
         System.out.println(minhaPilha);
         System.out.println(minhaPilha.pop());
         System.out.println(minhaPilha);
 
         minhaPilha.push(new No(99));
         System.out.println(minhaPilha);
-
-
-
 
     }
 }
