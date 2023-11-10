959ebe16d7b1d96b540c299a68043851416f4d96
lleonardx <lleonardx.alves@gmail.com>
2023-11-10 14:27:11 -0400

    implementando metodo getNo and get

--- lista-duplamente-encadeada/src/main/java/one/digitalinnovation/ListaDuplamenteEncadeada.java
+++ lista-duplamente-encadeada/src/main/java/one/digitalinnovation/ListaDuplamenteEncadeada.java
@@ -6,17 +6,27 @@ public class ListaDuplamenteEncadeada <T> {
 
     private int tamanhoLista;
 
-    public int ListaDuplamenteEncadeada(){
+    public ListaDuplamenteEncadeada(){
         this.primeiroNo = null;
         this.ultimoNo = null;
         tamanhoLista = 0;
     }
 
+    public T get(int index){
+        return this.getNo(index).getConteudo();
+    }
+
+    private NoDuplo<T> getNo(int index){
+        NoDuplo<T> noAuxiliar = primeiroNo;
+
+        for(int i=0;(i<index) && (noAuxiliar!=null); i++){
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+        return noAuxiliar;
+    }
+
     public int size(){
         return tamanhoLista;
     }
-
-
-
 
 }
