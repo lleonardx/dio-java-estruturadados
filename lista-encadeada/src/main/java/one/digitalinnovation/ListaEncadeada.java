9fc0c10e1c7ea442ec0e96441b54eecd93f7c7ef
lleonardx <lleonardx.alves@gmail.com>
2023-11-10 09:55:52 -0400

    implementação do metodo toString

--- lista-encadeada/src/main/java/one/digitalinnovation/ListaEncadeada.java
+++ lista-encadeada/src/main/java/one/digitalinnovation/ListaEncadeada.java
@@ -85,15 +85,15 @@ public class ListaEncadeada <T> {
         }
     }
 
-
-
-
-
-
-
-
-
-
-
-
+    @Override
+    public String toString() {
+        String strRetorno = "";
+        No<T> noAuxiliar = referenciaEntrada;
+        for(int i=0; i <this.size();i++){
+            strRetorno += "[No{conteudo=" + noAuxiliar.getConteudo() + "}--->";
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+        strRetorno += "null";
+        return strRetorno;
+    }
 }
