68a6d334525be040b655e0b32ff5ed7a030db4b1
lleonardx <lleonardx.alves@gmail.com>
2023-11-10 09:35:35 -0400

    implementação do metodo add e validaIndice

--- lista-encadeada/src/main/java/one/digitalinnovation/ListaEncadeada.java
+++ lista-encadeada/src/main/java/one/digitalinnovation/ListaEncadeada.java
@@ -8,8 +8,35 @@ public class ListaEncadeada <T> {
         this.referenciaEntrada = null;
     }
 
+    public void add(T conteudo){
+        No<T> novoNo = new No<>(conteudo);
+
+        if(this.isEmpty()){
+            referenciaEntrada = novoNo;
+            return;
+        }
+
+        No<T> noAuxiliar = referenciaEntrada;
+        for(int i = 0; i<this.size()-1;i++){
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+
+        noAuxiliar.setProximoNo(novoNo);
+    }
+
+    private No<T> getNo(int index){
+        validaIndice(index);
+        No<T> noAuxiliar = referenciaEntrada;
+        No<T> noRetorno = null;
+        for(int i=0; i<this.size()-1;i++){
+            noRetorno = noAuxiliar;
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+        return noRetorno;
+    }
+
     public int size(){
-        int tamanhoLista = 0
+        int tamanhoLista = 0;
         No<T> referenciaAux = referenciaEntrada;
         while(true){
             if(referenciaAux != null){
@@ -25,6 +52,14 @@ public class ListaEncadeada <T> {
         }
         return tamanhoLista;
     }
+
+    private void validaIndice(int index){
+        if(index >= size()){
+            int ultimoIndice = size() -1;
+            throw new IndexOutOfBoundsException("Não existe conteuno no indice" + index + " desta lista. Esta lista do vai até o indice:  " + ultimoIndice + "." );
+        }
+    }
+
     public boolean isEmpty(){
         if(referenciaEntrada != null){
             return true;
