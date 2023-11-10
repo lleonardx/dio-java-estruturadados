53e61c80e3fbeb88307e66b4b1240052abe6d782
lleonardx <lleonardx.alves@gmail.com>
2023-11-10 08:24:19 -0400

    refatorando Fila

--- Fila/src/main/java/one/digitalinnovation/Fila.java
+++ Fila/src/main/java/one/digitalinnovation/Fila.java
@@ -7,12 +7,13 @@ public class Fila {
         this.refNoEntradaFila = null;
     }
 
-    public void enqueue(No novoNo) {
+    public void enqueue(Object obj) {
+        No novoNo = new No(obj);
         novoNo.setRefNo(refNoEntradaFila);
         refNoEntradaFila = novoNo;
     }
 
-    public No dequeue() {
+    public Object dequeue() {
         if (!this.isEmpty()) {
             No primeiroNo = refNoEntradaFila;
             No noAuxiliar = refNoEntradaFila;
@@ -25,12 +26,12 @@ public class Fila {
                     break;
                 }
             }
-            return primeiroNo;
+            return primeiroNo.getObject();
         }
         return null;
     }
 
-    public No first(){
+    public Object first(){
         if(!this.isEmpty()){
             No primeiroNo = refNoEntradaFila;
             while(true){
@@ -40,7 +41,7 @@ public class Fila {
                     break;
                 }
             }
-            return primeiroNo;
+            return primeiroNo.getObject();
         }
         return null;
     }
