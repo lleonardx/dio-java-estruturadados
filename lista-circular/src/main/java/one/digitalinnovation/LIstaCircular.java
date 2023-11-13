a2abd3a5f16fcd055089755a6b0c615ad11f1aba
lleonardx <lleonardx.alves@gmail.com>
2023-11-13 08:29:55 -0400

    implementando metodo get e getno

--- lista-circular/src/main/java/one/digitalinnovation/LIstaCircular.java
+++ lista-circular/src/main/java/one/digitalinnovation/LIstaCircular.java
@@ -2,8 +2,24 @@ package one.digitalinnovation;
 
 public class LIstaCircular<T> {
     private No<T> cabeca;
-    private No<T> calda;
+    private No<T> cauda;
     private int tamanhoLista;
+
+    public T get(int index){
+        return this.getNo(index).getConteudo();
+    }
+    private No<T> getNo(int index){
+        if(this.isEmpty())
+            throw new IndexOutOfBoundsException("A lista está vazia.")
+        if(index == 0){
+            return this.cauda;
+        }
+        No<T> noAUxiliar = this.cauda;
+        for(int i=0;(i<index) && (noAUxiliar != null);i++){
+            noAUxiliar = noAUxiliar.getNoProximo();
+        }
+        return noAUxiliar;
+    }
 
     public boolean isEmpty(){
         return this.tamanhoLista == 0 ? true : false;
