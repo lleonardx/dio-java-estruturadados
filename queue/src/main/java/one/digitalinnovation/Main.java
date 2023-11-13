de0e8165f26a3fb0c8e530c0dcfbbf10145f8473
lleonardx <lleonardx.alves@gmail.com>
2023-11-13 13:36:14 -0400

    utilizando queue

---
+++ queue/src/main/java/one/digitalinnovation/Main.java
@@ -0,0 +1,34 @@
+package one.digitalinnovation;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) {
+        Queue<Carro> queueCarro = new LinkedList<>();
+
+        queueCarro.add(new Carro("Ford"));
+        queueCarro.add(new Carro("Chevrolet"));
+        queueCarro.add(new Carro("Fiat"));
+
+        System.out.println(queueCarro.add(new Carro("Pegeout")));
+        System.out.println(queueCarro);
+
+        //Retorna
+        System.out.println(queueCarro.offer(new Carro("Renault")));
+        System.out.println(queueCarro);
+
+        System.out.println(queueCarro.peek());
+        System.out.println(queueCarro);
+
+        System.out.println(queueCarro.poll());
+        System.out.println(queueCarro);
+
+        System.out.println(queueCarro.isEmpty());
+        System.out.println(queueCarro.size());
+
+
+
+
+    }
+}
