02afe289105f65e5cc6cc200509f48480bff9ab0
lleonardx <lleonardx.alves@gmail.com>
2023-11-13 09:04:07 -0400

    implementando metodo tostring

--- lista-circular/src/main/java/one/digitalinnovation/LIstaCircular.java
+++ lista-circular/src/main/java/one/digitalinnovation/LIstaCircular.java
@@ -62,4 +62,17 @@ public class LIstaCircular<T> {
     public int size(){
         return this.tamanhoLista;
     }
+
+    @Override
+    public String toString() {
+        String strRetorno = "";
+
+        No<T> noAuxiliar = this.cauda;
+        for(int i=0;i<this.size();i++){
+            strRetorno += "[No{conteudo=" + noAuxiliar.getConteudo() + "}]--->";
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+        strRetorno += this.size() != 0 ? "(Retorna ao Inicio)" : "[]";
+        return strRetorno;
+    }
 }
