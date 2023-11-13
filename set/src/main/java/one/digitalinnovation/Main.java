9758774a43815501c6b04edc29e6ea6179444d5d
lleonardx <lleonardx.alves@gmail.com>
2023-11-13 13:58:58 -0400

    utilizando set

---
+++ set/src/main/java/one/digitalinnovation/Main.java
@@ -0,0 +1,34 @@
+package one.digitalinnovation;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<Carro> hashSetCarros = new HashSet<>();
+
+        hashSetCarros.add(new Carro("Ford"));
+        hashSetCarros.add(new Carro("Chevrolet"));
+        hashSetCarros.add(new Carro("Fiat"));
+        hashSetCarros.add(new Carro("Peugeot"));
+        hashSetCarros.add(new Carro("Zip"));
+        hashSetCarros.add(new Carro("Alfa Romeo"));
+
+        System.out.println(hashSetCarros);
+
+        Set<Carro> treeSetCarros = new TreeSet<>();
+
+        hashSetCarros.add(new Carro("Ford"));
+        hashSetCarros.add(new Carro("Chevrolet"));
+        hashSetCarros.add(new Carro("Fiat"));
+        hashSetCarros.add(new Carro("Peugeot"));
+        hashSetCarros.add(new Carro("Zip"));
+        hashSetCarros.add(new Carro("Alfa Romeo"));
+
+        System.out.println(treeSetCarros);
+
+
+
+
+
+    }
+}
